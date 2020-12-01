be71b4e90644daa5b83296e6d4ae9d03e551ab29
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-01 14:25:26 -0800

    destructurename

--- name.js
+++ name.js
@@ -4,7 +4,7 @@ const spot = { name: 'spot', age: 5, weight: '20 lbs' }
 const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
 
 
-const getName = character.name;
+const { getName } = spot;
 
 module.exports = {
     spot,
