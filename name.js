27c0149be43b1b8cd6eff694b83ddae97ab7acf6
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-01 14:38:05 -0800

    refactorname

--- name.js
+++ name.js
@@ -1,13 +1,14 @@
+
+
 const spot = { name: 'spot', age: 5, weight: '20 lbs' }
 
 
-const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+
 
 
 const { getName } = spot;
 
 module.exports = {
     spot,
-    character,
     getName
 }
