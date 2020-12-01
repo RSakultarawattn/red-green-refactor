8328f51ab2e0c799cd9b2a5fce0039baeb52a107
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-01 14:17:44 -0800

    bracketname

---
+++ name.js
@@ -0,0 +1,13 @@
+const spot = { name: 'spot', age: 5, weight: '20 lbs' }
+
+
+//const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
+
+
+const getName = spot['name'];
+
+module.exports = {
+    spot,
+    character,
+    getName
+}
